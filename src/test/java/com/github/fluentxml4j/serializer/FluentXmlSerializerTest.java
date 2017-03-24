b4cef3d79dd817dfebadb0f3f81e0fe18d37c461
conni <c.buschka@arcusx.com>
2017-03-24 16:48:54 +0100

    Added test for serializer customization.

--- src/test/java/com/github/fluentxml4j/serializer/FluentXmlSerializerTest.java
+++ src/test/java/com/github/fluentxml4j/serializer/FluentXmlSerializerTest.java
@@ -2,14 +2,11 @@ package com.github.fluentxml4j.serializer;
 
 import com.github.fluentxml4j.junit.DocumentTestRule;
 import org.junit.ClassRule;
-import org.junit.Rule;
 import org.junit.Test;
-import org.w3c.dom.Document;
 
-import java.io.ByteArrayInputStream;
-import java.io.UnsupportedEncodingException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
 
-import static com.github.fluentxml4j.parser.FluentXmlParser.parse;
 import static com.github.fluentxml4j.serializer.FluentXmlSerializer.serialize;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
@@ -17,13 +14,32 @@ import static org.junit.Assert.assertThat;
 public class FluentXmlSerializerTest
 {
 	@ClassRule
-	public static DocumentTestRule documentTestRule = new DocumentTestRule("<test/>");
+	public static DocumentTestRule documentTestRule = new DocumentTestRule("<foo:test xmlns:foo=\"bar\"/>");
 
 	@Test
-	public void serializeToString()
+	public void serializeWithDefaultsToString()
 	{
 		String serializedXml = serialize(documentTestRule.document()).toString();
 
-		assertThat(serializedXml, is("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n<test/>\n"));
+		assertThat(serializedXml, is("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n<foo:test xmlns:foo=\"bar\"/>\n"));
 	}
+
+	@Test
+	public void serializeCustomizedToString()
+	{
+		String serializedXml = serialize(documentTestRule.document())
+				.withSerializer(new SerializerConfigurerAdapter()
+				{
+					@Override
+					protected void configure(Transformer transformer)
+					{
+						super.configure(transformer);
+						transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+					}
+				})
+				.toString();
+
+		assertThat(serializedXml, is("<foo:test xmlns:foo=\"bar\"/>\n"));
+	}
+
 }
